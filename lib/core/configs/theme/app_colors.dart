83630aa507b5fae923830516d15a22d3ba774db2
abdul23lm <abdul23lm@gmail.com>
2024-07-21 01:14:48 +0700

    feat(theme): setup app colors and theme. Also create any folders for future code with clean architecture

---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0XFF9B51E0);
+  static const lightBackground = Color(0XFFF5F5F5);
+  static const darkBackground = Color(0XFF121212);
+  static const grey = Color(0XFFB3B3B3);
+  static const darkGrey = Color(0XFF2A2A2A);
+}
